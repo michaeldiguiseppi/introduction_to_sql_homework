f00e38ae70450f9f6b73d55b0ba28e81f0e9b7ed
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-02-23 13:02:11 -0700

    Added comments delineating the code

--- commands.sql
+++ commands.sql
@@ -1,3 +1,4 @@
+/* Insert 5 rows into the database */
 INSERT INTO urls
     VALUES (default, 'http://www.google.com', default),
 (default, 'http://www.facebook.com', 1),
@@ -5,15 +6,20 @@ INSERT INTO urls
 (default, 'http://www.freeonlinegames.com', 2),
 (default, 'http://www.galvanize.com', 5);
 
-
+/* Select all rows in table */
 SELECT * FROM urls;
 
+/* Select all rows only original_id column */
 SELECT original_url FROM urls;
 
+/* Select one row by id */
 SELECT * FROM urls WHERE id = 2;
 
+/* Select one row by original_url */
 SELECT * FROM urls WHERE original_url = 'http://www.github.com';
 
+/* Update count on one row by id */
 UPDATE urls SET count = 10 WHERE id = 5;
 
+/* Delete one row by original_url */
 DELETE FROM urls WHERE original_url = 'http://www.freeonlinegames.com';
--- schema.sql
+++ schema.sql
@@ -1,7 +1,10 @@
+/* Create database */
 CREATE DATABASE url_shortener_mike_diguiseppi;
 
+/* If the table exists, drop it */
 DROP TABLE IF EXISTS urls;
 
+/* Create the new table with the right column names, types, defaults, etc. */
 CREATE TABLE urls (
 id serial primary key,
 original_url varchar(255) not null,
